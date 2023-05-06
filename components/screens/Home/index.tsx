29541a93b65ee17efcc64dd99e290c1b03dd36be
M M Akhtar <contact.mmakhtar@gmail.com>
2023-05-07 00:45:33 +0530

    adds header background

--- components/screens/Home/index.tsx
+++ components/screens/Home/index.tsx
@@ -5,23 +5,26 @@ import feRequests from '../../../connections/fe-requests';
 
 const HomePage = () => {
     return (
-        <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+        <>
             <View style={styles.header}>
                 <Text style={styles.title}>Movie App</Text>
             </View>
-            <MovieRow title='Action Movies' fetchUrl={feRequests.fetchActionMovies} />
-            <MovieRow title='Action Shows' fetchUrl={feRequests.fetchActionShows} />
-            <MovieRow title='Animated Shows' fetchUrl={feRequests.fetchAnimationShows} />
-            <MovieRow title='Comedy Movies' fetchUrl={feRequests.fetchComedyMovies} />
-            <MovieRow title='Comedy Shows' fetchUrl={feRequests.fetchComedyShows} />
-            <MovieRow title='Crime SHows' fetchUrl={feRequests.fetchCrimeShows} />
-            <MovieRow title='Documentaries' fetchUrl={feRequests.fetchDocumentries} />
-            <MovieRow title='Bollywood Movies' fetchUrl={feRequests.fetchHindiMovies} />
-            <MovieRow title='Horror Movies' fetchUrl={feRequests.fetchHorrorMovies} />
-            <MovieRow title='Only on Netflix' fetchUrl={feRequests.fetchNetflixOriginals} />
-            <MovieRow title='Romance Movies' fetchUrl={feRequests.fetchRomanceMovies} />
-            <MovieRow title='SciFi Movies' fetchUrl={feRequests.fetchScifiMovies} />
-        </ScrollView>
+            <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+                <MovieRow title='Action Movies' fetchUrl={feRequests.fetchActionMovies} />
+                <MovieRow title='Action Shows' fetchUrl={feRequests.fetchActionShows} />
+                <MovieRow title='Animated Shows' fetchUrl={feRequests.fetchAnimationShows} />
+                <MovieRow title='Comedy Movies' fetchUrl={feRequests.fetchComedyMovies} />
+                <MovieRow title='Comedy Shows' fetchUrl={feRequests.fetchComedyShows} />
+                <MovieRow title='Crime SHows' fetchUrl={feRequests.fetchCrimeShows} />
+                <MovieRow title='Documentaries' fetchUrl={feRequests.fetchDocumentries} />
+                <MovieRow title='Bollywood Movies' fetchUrl={feRequests.fetchHindiMovies} />
+                <MovieRow title='Horror Movies' fetchUrl={feRequests.fetchHorrorMovies} />
+                <MovieRow title='Only on Netflix' fetchUrl={feRequests.fetchNetflixOriginals} />
+                <MovieRow title='Romance Movies' fetchUrl={feRequests.fetchRomanceMovies} />
+                <MovieRow title='SciFi Movies' fetchUrl={feRequests.fetchScifiMovies} />
+                <View style={styles.bottom}></View>
+            </ScrollView>
+        </>
     )
 }
 
--- components/screens/Home/styles.ts
+++ components/screens/Home/styles.ts
@@ -3,23 +3,32 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        height: 'auto'
+        // height: 'auto',
+        paddingTop: 120,
+        paddingBottom: 400,
     },
     title: {
-        fontSize: 40,
+        fontSize: 26,
         textTransform: 'uppercase',
-        letterSpacing: 4,
-        fontWeight: 'bold',
-        color: '#fff',
+        letterSpacing: 10,
+        fontWeight: '400',
+        color: 'rgba(255,255,255,0.9)',
         marginTop: 'auto',
         marginBottom: 'auto',
         alignSelf: 'center'
     },
     header: {
-        height: 200,
-        paddingTop:50,
-        backgroundColor: 'rgba(143,7,14,0.2)',
-        marginBottom:20
+        position: 'absolute',
+        width: '100%',
+        zIndex: 1,
+        height: 100,
+        paddingTop: 40,
+        backgroundColor: 'rgba(143,7,14,0.92)',
+        marginBottom: 20,
+    },
+    bottom: {
+        height: 120,
+        
     }
 })
 
